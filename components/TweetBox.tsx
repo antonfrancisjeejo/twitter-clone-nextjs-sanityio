9766437fdf964fcb30fcd0e61c9ada05d535cb43
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-07-04 20:46:18 +0530

    Initial commit

---
+++ components/TweetBox.tsx
@@ -0,0 +1,149 @@
+import React, {
+  Dispatch,
+  MouseEvent,
+  SetStateAction,
+  useRef,
+  useState,
+} from "react";
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+import { Tweet, TweetBody } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
+import toast from "react-hot-toast";
+
+interface Props {
+  setTweets: Dispatch<SetStateAction<Tweet[]>>;
+}
+
+const TweetBox = ({ setTweets }: Props) => {
+  const [input, setInput] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+  const { data: session } = useSession();
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const addImageToTweet = (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (!imageInputRef.current?.value) return;
+
+    setImage(imageInputRef.current.value);
+    imageInputRef.current.value = "";
+    setImageUrlBoxIsOpen(false);
+  };
+
+  const postTweet = async () => {
+    const tweetInfo: TweetBody = {
+      text: input,
+      username: session?.user?.name || "Unknown User",
+      profileImg:
+        session?.user?.image ||
+        "https://www.itdp.org/wp-content/uploads/2021/06/avatar-man-icon-profile-placeholder-260nw-1229859850-e1623694994111.jpg",
+      image: image,
+    };
+
+    const result = await fetch(`/api/addTweet`, {
+      body: JSON.stringify(tweetInfo),
+      method: "POST",
+    });
+
+    await result.json();
+
+    const newTweets = await fetchTweets();
+
+    setTweets(newTweets);
+
+    toast("Tweet posted", {
+      icon: "🚀",
+    });
+  };
+
+  const handleSubmit = (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    postTweet();
+
+    setInput("");
+    setImage("");
+    setImageUrlBoxIsOpen(false);
+  };
+
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        className="h-14 w-14 rounded-full object-cover mt-4"
+        src={
+          session?.user?.image ||
+          "https://www.itdp.org/wp-content/uploads/2021/06/avatar-man-icon-profile-placeholder-260nw-1229859850-e1623694994111.jpg"
+        }
+        alt=""
+      />
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="What's Happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+          />
+          <div className="flex items-center">
+            <div className="flex space-x-2 text-twitter flex-1">
+              <PhotographIcon
+                onClick={() => setImageUrlBoxIsOpen((current) => !current)}
+                className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150"
+              />
+              <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+            </div>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              disabled={!input || !session}
+              className="bg-twitter px-5 py-2 font-bold text-white rounded-full disabled:opacity-40"
+            >
+              Tweet
+            </button>
+          </div>
+          {imageUrlBoxIsOpen && (
+            <div className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+              <input
+                type="text"
+                ref={imageInputRef}
+                placeholder="Enter Image Url"
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+              />
+              <button
+                onClick={addImageToTweet}
+                type="submit"
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </div>
+          )}
+          {image && (
+            <img
+              className="mt-10 h-40 w-full rounded-xl object-contain shadow-lg"
+              src={image}
+            />
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TweetBox;
